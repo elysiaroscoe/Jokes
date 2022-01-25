a27257ab37d98b23d97ccf50ad27cc501a21c2df
Roscoe <elysiaroscoe@gmail.com>
2022-01-25 11:21:10 -0800

    adjusting params

--- Routes/jokes.routes.js
+++ Routes/jokes.routes.js
@@ -2,9 +2,9 @@ const JokeController = require('../Controllers/jokes.controller');
 
 module.exports = app =>{
     app.get('/api/jokes', JokeController.findAllJokes);
-    app.get('/api/jokes/_id', JokeController.findOneJoke);
+    app.get('/api/jokes/:id', JokeController.findOneJoke);
     app.get('/api/jokes/random', JokeController.randomJoke);
     app.post('/api/jokes/new', JokeController.createNewJoke);
-    app.put('/api/jokes/:_id', JokeController.updateJoke);
-    app.delete('/api/jokes/delete/:_id', JokeController.deleteJoke)
+    app.put('/api/jokes/:id', JokeController.updateJoke);
+    app.delete('/api/jokes/delete/:id', JokeController.deleteJoke)
 }
